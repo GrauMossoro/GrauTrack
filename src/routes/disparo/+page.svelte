2907a980a8f9773a6f0a5de07c7f94be4e85da68
GrauMossoro <graueducacionalmossoro@gmail.com>
2026-03-12 19:31:48 +0000

    add: status do disparo "Em andamento"

--- src/routes/disparo/+page.svelte
+++ src/routes/disparo/+page.svelte
@@ -870,6 +870,10 @@
                           <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
                             Rejeitado
                           </span>
+                        {:else if blast.status === 'Em andamento'}
+                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">
+                          Em andamento
+                        </span>
                         {:else}
                           <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
                             Aprovado
